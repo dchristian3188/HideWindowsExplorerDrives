0b78f612afb07cf80946eb2c884e9716c10ee631
dchristian3188 <dchristian3188@gmail.com>
2017-08-20 18:09:34 -0700

    Updated Partition

--- TypeExtensions/Partition.ps1
+++ TypeExtensions/Partition.ps1
@@ -1,31 +1,35 @@
-$type = 'Microsoft.Management.Infrastructure.CimInstance#ROOT/Microsoft/Windows/Storage/MSFT_Partition'
-
-$TypeData = @{
-    TypeName = $type
-    MemberType = 'ScriptProperty'
-    MemberName = 'IsHidden'
-    Value = {
-       (Get-DriveStatus -DriveLetter $this.DriveLetter).IsHidden
+if (Get-Command -Name Get-Partition)
+{
+    $type = 'Microsoft.Management.Infrastructure.CimInstance#ROOT/Microsoft/Windows/Storage/MSFT_Partition'
+    
+    $TypeData = @{
+        TypeName   = $type
+        MemberType = 'ScriptProperty'
+        MemberName = 'IsHidden'
+        Value      = {
+            (Get-DriveStatus -DriveLetter $this.DriveLetter).IsHidden
+        }
     }
-}
-Update-TypeData @TypeData -Force
-
-$TypeData = @{
-    TypeName = $type
-    MemberType = 'ScriptMethod'
-    MemberName = 'HidePartition'
-    Value = {
-        Hide-DriveLetter -DriveLetter $this.DriveLetter
+    Update-TypeData @TypeData -Force
+    
+    $TypeData = @{
+        TypeName   = $type
+        MemberType = 'ScriptMethod'
+        MemberName = 'HidePartition'
+        Value      = {
+            Hide-DriveLetter -DriveLetter $this.DriveLetter
+        }
     }
-}
-Update-TypeData @TypeData -Force
-
-$TypeData = @{
-    TypeName = $type
-    MemberType = 'ScriptMethod'
-    MemberName = 'ShowPartition'
-    Value = {
-        Show-DriveLetter -DriveLetter $this.DriveLetter
+    Update-TypeData @TypeData -Force
+    
+    $TypeData = @{
+        TypeName   = $type
+        MemberType = 'ScriptMethod'
+        MemberName = 'ShowPartition'
+        Value      = {
+            Show-DriveLetter -DriveLetter $this.DriveLetter
+        }
     }
+    Update-TypeData @TypeData -Force
+    
 }
-Update-TypeData @TypeData -Force
